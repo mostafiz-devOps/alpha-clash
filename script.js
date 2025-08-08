13f678b419897ebcd08c204f1e3fc2b3ebe8d549
masud-k <contact.masud99@gmail.com>
2025-08-08 23:27:49 +0600

    dynamic function added to play button

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function togglePlayGround() {
+  const playGround = document.querySelector('.play-ground');
+  const homeSection = document.querySelector('.home');
+  const scoreSection = document.querySelector('.score');
+
+  if (playGround.classList.contains('hidden')) {
+    playGround.classList.remove('hidden');
+    homeSection.classList.add('hidden');
+    scoreSection.classList.add('hidden');
+  } else {
+    playGround.classList.add('hidden');
+    homeSection.classList.remove('hidden');
+    scoreSection.classList.remove('hidden');
+  }
+}
