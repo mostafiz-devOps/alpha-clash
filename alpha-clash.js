1cc92932ecf71c1d6ea60bd11740137fa0a7b848
Mostafizur Rahman <mostafizuroffical@outlook.com>
2025-08-11 20:41:33 +0600

    do it Masud-k

---
+++ alpha-clash.js
@@ -0,0 +1,7 @@
+function togglePlayGround() {
+  const playHome = document.getElementById("home");
+  playHome.classList.add("hidden");
+
+  const playGround = document.getElementById("play-ground");
+  playGround.classList.remove("hidden");
+}
